0592a32bc89480123c55e9c791c05bad9c9bc79b
Derek <chenjunqi07@icloud.com>
2020-11-18 23:25:10 +0800

    Initial Commit

---
+++ BMS3.5/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  BMS3.5
+//
+//  Created by Derek Chan on 2020/11/18.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
