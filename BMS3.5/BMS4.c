2a51ae643d614f266ea7337dcd5c97be18e1017f
Derek <chenjunqi07@icloud.com>
2020-11-28 10:57:37 +0800

    #update

--- BMS3.5/BMS4.c
+++ BMS3.5/BMS4.c
@@ -6,4 +6,47 @@
 //
 
 #include "BMS4.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "SignUp:In.h"
 
+//MARK: -开始界面
+void startSystem() {
+    int cmd, end = 0;
+    while (1) {
+        system("cls");
+        printf("\n\n\n\n\n\n\n");
+        printf("**************************************************************");
+        printf("\n图书馆管理系统\n");
+        printf("1.管理员登陆\n");
+        printf("2.管理员注册\n");
+        printf("0.退出\n");
+        printf("**************************************************************");
+        printf("\n\n\n\n\n\n\n");
+        printf("输入你需要进行的操作: ");
+        scanf("%d", &cmd);
+        getchar();
+        switch (cmd)
+        {
+        case 1:
+            adminLogin();
+            break;
+        case 2:
+            adminRegister();
+            break;
+        case 0:
+            end = 1;
+            break;
+        default:
+            printf("错误输入!\n");
+        }
+        if (end == 1)
+            break;
+    }
+}
+
+int main() {
+    startSystem();
+    return 0;
+}
--- BMS3.5/BMS4.h
+++ BMS3.5/BMS4.h
@@ -21,6 +21,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include "SignUp:In.h"
 
 //管理员信息
 typedef struct AdminInfo {
@@ -45,164 +46,165 @@ struct AdminInfo *adminHead = NULL, *adminEnd = NULL;
 struct BookInfo *bookHead = NULL, *bookEnd = NULL;
 int adminCount, bookCount;
 
-//MARK: -开始界面（待完善）
-void startSystem() {
-    int cmd, end = 0;
-    while (1) {
-        <#statements#>
-    }
-}
-
-//管理员操作菜单
-void adminFunction() {
-    int cmd;
-    int end = 0;
- 
-    while (1) {
-        system("cls");
-        printf("\n\n\n\n\n\n\n");
-        printf("**************************************************************");
-        printf("\n图书馆管理员功能系统\n");
-        printf("1.添加书籍信息\n");
-        printf("2.删除书籍信息\n");
-        printf("3.修改书籍信息\n");
-        printf("4.查询书籍信息\n");
-        printf("5.查询用户信息\n");
-        printf("0.退出\n");
-        printf("**************************************************************");
-        printf("\n\n\n\n\n\n\n");
-        printf("输入您需要进行的操作: ");
-        scanf("%d", &cmd);
-        getchar();
-//        switch (cmd)
-//        {
-//        case 1:
-//            addbook();
-//            break;
-//        case 2:
-//            deletebook();
-//            break;
-//        case 3:
-//            changebook();
-//            break;
-//        case 4:
-//            searchbook();
-//            break;
-//        case 5:
-//            searchuser();
-//            break;
-//        case 0:
-//            end = 1;
-//            break;
-//        default:
-//            printf("错误的输入!\n");
-//        }
-        if (end == 1)
-            break;
-    }
-}
-
-//管理员登陆
-void adminLogin() {
-    Admin *p;
-    char adminName[100];
-    char adminPassword[100];
-    
-    p = adminHead;
-    system("cls");
-    printf("\n\n\n\n\n\n\n");
-    printf("**************************************************************");
-    printf("\n输入您的管理员信息: \n");
-    printf("管理员姓名: ");
-    gets(adminName);
-    printf("管理员密码: ");
-    gets(adminPassword);
-    printf("**************************************************************");
-    printf("\n\n\n\n\n\n\n");
-    
-    while (p != NULL) {
-        if (strcmp(p->adminName, adminName) == 0) {
-            while (strcmp(p->scert, adminPassword) != 0) {
-                printf("密码错误!请重新输入\n");
-                printf("adminscert: ");
-                gets(adminPassword);
-            }
-            printf("管理员%s登陆成功!将自动跳转至用户界面...", p->adminName);
-            adminFunction();
-        }
-        p = p->next;
-    }
-}
-
-//管理员账户注册
-void adminRegister(){
-    Admin *p;
-    char adminName[1000];
-    char adminPassword[1000];
+void writefile(int flag) {
     FILE *fp;
-    
-    system("cls");
-    if (adminCount == 0) {//第一次添加
-        adminHead = (struct AdminInfo *)malloc(sizeof(struct AdminInfo));
-        printf("adminname: ");
-        while (gets(adminName)) {
-            if (strlen(adminName) < 10) {
-                break;
-            } else {
-                printf("too long");
-            }
+    Admin *adminp;
+    Book *bookp;
+ 
+    adminp = adminHead;
+    bookp = bookHead;
+ 
+    if (flag == 1) {
+        fp = fopen("admin", "w");
+        while (adminp != NULL){
+            fwrite(adminp, sizeof(Admin), 1, fp);
+            adminp = adminp->next;
         }
-        strcpy(adminHead->adminName, adminName);
-        printf("adminscert: ");
-        while (gets(adminPassword)) {
-            if (strlen(adminPassword) < 10) {
-                break;
-            } else {
-                printf("too long");
-            }
-        }
-        strcpy(adminHead->scert, adminPassword);
-        adminEnd = adminHead;
-        adminEnd->next = NULL;
     } else {
-        p = (struct AdminInfo *)malloc(sizeof(struct AdminInfo));
-        printf("管理员姓名: ");
-        while (gets(adminName)) {
-            if (strlen(adminName) < 10) {
-                break;
-            } else {
-                printf("too long");
-            }
+        fp = fopen("book", "w");
+        while (bookp != NULL) {
+            fwrite(bookp, sizeof(Book), 1, fp);
+            bookp = bookp->next;
         }
-        strcpy(p->adminName, adminName);
-        printf("管理员密码: ");
-        while (gets(adminPassword)) {
-            if (strlen(adminPassword) < 10) {
-                break;
-            } else {
-                printf("too long");
-            }
-        }
-        strcpy(p->scert, adminPassword);
-        adminEnd->next = p;
-        adminEnd = p;
-        adminEnd->next = NULL;
-    }
-    adminCount++;
-    
-    fp = fopen("admin", "w");
-    p = adminHead;
-    while (p != NULL) {
-        fwrite(p, sizeof(struct AdminInfo), 1, fp);
-        p = p->next;
     }
     fclose(fp);
-    
-    system("cls");
-    printf("管理员%s创建成功\n", adminEnd->adminName);
 }
 
-//从键盘键入书籍信息
+void readadminfile(){
+    Admin *p;
+    FILE *fp;
+    
+    p = adminHead;
+    fp = fopen("admin", "ab+");
+    while (fread(p, sizeof(Admin), 1, fp)){
+        if (p->next != NULL){
+            p = (Admin*)malloc(sizeof(Admin));
+            
+            adminEnd->next = p;
+            adminEnd = p;
+            adminEnd->next = NULL;
+        }
+    }
+}
+
+void readbookfile(){
+    Book *p;
+    FILE *fp;
+    
+    p = bookHead;
+    fp = fopen("book", "ab+");
+    while (fread(p, sizeof(Book), 1, fp)){
+        if (p->next != NULL){
+            p = (Book*)malloc(sizeof(Book));
+            
+            bookEnd->next = p;
+            bookEnd = p;
+            bookEnd->next = NULL;
+        }
+    }
+}
+
+////MARK: -管理员登陆
+//void adminLogin() {
+//    Admin *p;
+//    char adminName[100];
+//    char adminPassword[100];
+//
+//    p = adminHead;
+//    system("cls");
+//    printf("\n\n\n\n\n\n\n");
+//    printf("**************************************************************");
+//    printf("\n输入您的管理员信息: \n");
+//    printf("管理员姓名: ");
+//    gets(adminName);
+//    printf("管理员密码: ");
+//    gets(adminPassword);
+//    printf("**************************************************************");
+//    printf("\n\n\n\n\n\n\n");
+//
+//    while (p != NULL) {
+//        if (strcmp(p->adminName, adminName) == 0) {
+//            while (strcmp(p->scert, adminPassword) != 0) {
+//                printf("密码错误!请重新输入\n");
+//                printf("adminscert: ");
+//                gets(adminPassword);
+//            }
+//            printf("管理员%s登陆成功!将自动跳转至用户界面...", p->adminName);
+//            adminFunction();
+//        }
+//        p = p->next;
+//    }
+//}
+//
+////MARK: -管理员账户注册
+//void adminRegister(){
+//    Admin *p;
+//    char adminName[1000];
+//    char adminPassword[1000];
+//    FILE *fp;
+//
+//    system("cls");
+//    if (adminCount == 0) {//第一次添加
+//        adminHead = (struct AdminInfo *)malloc(sizeof(struct AdminInfo));
+//        printf("adminname: ");
+//        while (gets(adminName)) {
+//            if (strlen(adminName) < 10) {
+//                break;
+//            } else {
+//                printf("too long");
+//            }
+//        }
+//        strcpy(adminHead->adminName, adminName);
+//        printf("adminscert: ");
+//        while (gets(adminPassword)) {
+//            if (strlen(adminPassword) < 10) {
+//                break;
+//            } else {
+//                printf("too long");
+//            }
+//        }
+//        strcpy(adminHead->scert, adminPassword);
+//        adminEnd = adminHead;
+//        adminEnd->next = NULL;
+//    } else {
+//        p = (struct AdminInfo *)malloc(sizeof(struct AdminInfo));
+//        printf("管理员姓名: ");
+//        while (gets(adminName)) {
+//            if (strlen(adminName) < 10) {
+//                break;
+//            } else {
+//                printf("too long");
+//            }
+//        }
+//        strcpy(p->adminName, adminName);
+//        printf("管理员密码: ");
+//        while (gets(adminPassword)) {
+//            if (strlen(adminPassword) < 10) {
+//                break;
+//            } else {
+//                printf("too long");
+//            }
+//        }
+//        strcpy(p->scert, adminPassword);
+//        adminEnd->next = p;
+//        adminEnd = p;
+//        adminEnd->next = NULL;
+//    }
+//    adminCount++;
+//
+//    fp = fopen("admin", "w");
+//    p = adminHead;
+//    while (p != NULL) {
+//        fwrite(p, sizeof(struct AdminInfo), 1, fp);
+//        p = p->next;
+//    }
+//    fclose(fp);
+//
+//    system("cls");
+//    printf("管理员%s创建成功\n", adminEnd->adminName);
+//}
+
+//MARK: -从键盘键入书籍信息
 void TypingInfo() {
     Book *p;
     char title[100];
@@ -295,7 +297,7 @@ void TypingInfo() {
     printf("书籍添加成功\n");
 }
 
-//查询图书信息
+//MARK: -查询图书信息
 void QueryBook() {
     Book *p;
     char title[100];
@@ -336,7 +338,7 @@ void QueryBook() {
     }
 }
 
-//删除图书信息
+//MARK: -删除图书信息
 void DeleteBookInfo() {
     Book *p;
     char title[100];
@@ -372,12 +374,146 @@ void DeleteBookInfo() {
         p = p->next;
     }
     p = NULL;
-    //TODO: -
+    writefile(2);
 }
 
-//修改图书信息
+//MARK: -修改图书信息
 void ModifyBookInfo() {
+    Book *p;
+    char title[100], bookAuthur[100];
+    int exist = 0, end = 0;
+    int bYear, bMonth, bDay, bID, count;
     
+    p = bookHead;
+    system("cls");
+    printf("booklist:\n");
+    while (p != NULL)
+    {
+        printf("bookname: %10s\n", p->bookname);
+        p = p->next;
+    }
+    
+    p = bookHead;
+    printf("输入你需要修改的书籍名称(按@退出): ");
+    while (gets(title)){
+        if (strcmp(title, "@") == 0) {
+            break;
+        }
+        while (p != NULL){
+            if (strcmp(p->bookname, title) == 0){
+                exist = 1;
+                break;
+            }
+            p = p->next;
+        }
+        if (!exist){
+            printf("不存在该书籍名称，请重新输入: ");
+        } else {
+            break;
+        }
+    }
+    if (!exist) {
+        return;
+    } else {
+        int cmd;
+        
+        system("cls");
+        printf("\n\n\n\n\n\n\n");
+        printf("**************************************************************\n");
+        printf("1.修改书籍名称\n");
+        printf("2.修改书籍id号\n");
+        printf("3.修改书籍日期\n");
+        printf("4.修改书籍数量\n");
+        printf("5.修改书籍作者\n");
+        printf("0.退出\n");
+        printf("**************************************************************");
+        printf("\n\n\n\n\n\n\n");
+        while (1){
+            printf("输入您需要进行的操作: ");
+            scanf("%d", &cmd);
+            switch (cmd){
+                case 1:
+                    printf("输入你修改后的书籍名称: ");
+                    scanf("%s", title);
+//                    changuserbook(p->bookname,title);
+                    strcpy(p->bookname, title);
+                    break;
+                case 2:
+                    printf("输入你修改后的书籍ID: ");
+                    scanf("%d", &bID);
+                    p->id = bID;
+                    break;
+                case 3:
+                    printf("输入你修改后的书籍日期: ");
+                    scanf("%d%d%d", &bYear, &bMonth, &bDay);
+                    p->year = bYear;
+                    p->month = bMonth;
+                    p->day = bDay;
+                    break;
+                case 4:
+                    printf("输入你修改后的书籍数量: ");
+                    scanf("%d", &count);
+                    p->count = count;
+                    break;
+            case 5:
+                printf("输入你修改后的书籍作者: ");
+                scanf("%s", bookAuthur);
+                strcpy(p->author, bookAuthur);
+                break;
+            case 0:
+                end = 1;
+                break;
+            }
+            if (end == 1){
+                break;
+            }
+        }
+    }
+    writefile(2);
 }
+
+////管理员操作菜单
+//void adminFunction() {
+//    int cmd;
+//    int end = 0;
+//
+//    while (1) {
+//        system("cls");
+//        printf("\n\n\n\n\n\n\n");
+//        printf("**************************************************************");
+//        printf("\n图书馆管理员功能系统\n");
+//        printf("1.添加书籍信息\n");
+//        printf("2.删除书籍信息\n");
+//        printf("3.修改书籍信息\n");
+//        printf("4.查询书籍信息\n");
+//        printf("0.退出\n");
+//        printf("**************************************************************");
+//        printf("\n\n\n\n\n\n\n");
+//        printf("输入您需要进行的操作: ");
+//        scanf("%d", &cmd);
+//        getchar();
+//        switch (cmd){
+//            case 1:
+//                TypingInfo();
+//                break;
+//            case 2:
+//                DeleteBookInfo();
+//                break;
+//            case 3:
+//                ModifyBookInfo();
+//                break;
+//            case 4:
+//                QueryBook();
+//                break;
+//            case 0:
+//                end = 1;
+//                break;
+//            default:
+//            printf("错误的输入!\n");
+//        }
+//        if (end == 1)
+//            break;
+//    }
+//}
 
 #endif /* BMS4_h */
